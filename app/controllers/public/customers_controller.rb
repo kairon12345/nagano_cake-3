00af6b5fcbc0666ba9e70b1b55718905cb96c67e
EC2 Default User <ec2-user@ip-172-31-16-133.ec2.internal>
2022-07-11 15:55:44 +0000

    edit totyuu

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,20 +1,28 @@
 class Public::CustomersController < ApplicationController
 
+
   def show
-    @customer=Customer.all
+   @customer=current_customer
   end
 
   def edit
-    @customer=Customer.all
+    @customer=current_customer
   end
 
   def update
+    @customer=current_customer
+    @customer.uopdate
+    redirect_to customers_my_page_path
   end
 
   def unsubscript
   end
 
   def withdraw
+    @customer=current_customer
+    @customer.update(is_active: false)
+    reset_settion
+    redirect_to root_path
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,7 @@ module NaganoCake
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  # config/application.rb
+
 end
