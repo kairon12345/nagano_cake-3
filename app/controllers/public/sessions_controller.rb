f178f01e785e63235a4d7294d14029a740cd8d1f
EC2 Default User <ec2-user@ip-172-31-16-133.ec2.internal>
2022-07-12 14:56:35 +0000

    customer settinng

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  
-  # before_action :configure_sign_in_params, only: [:create]
 
+  # before_action :configure_sign_in_params, only: [:create]
+ before_action :reject_customer
   # GET /resource/sign_in
   # def new
   #   super
@@ -21,9 +21,21 @@ class Public::SessionsController < Devise::SessionsController
 
   # protected
 
+  def reject_user
+    @customer = customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_active == false)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to new_customer_registration
+      else
+        flash[:notice] = "項目を入力してください"
+      end
+    end
+  end
+
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
+
 end
